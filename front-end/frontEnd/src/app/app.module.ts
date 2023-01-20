bd279c09df24717f68e006fd772ff3f8321b3730
naolca <firaolibrahim28@gmail.com>
2023-01-20 22:40:23 +0300

    created the login component

--- front-end/frontEnd/src/app/app.module.ts
+++ front-end/frontEnd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './home-body/footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {path :'', component: HomeBodyComponent},
   {path :'HomeBody', component: HomeBodyComponent},
@@ -28,6 +29,7 @@ const routes: Routes = [
     SearchResultComponent,
     FooterComponent,
     AboutComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
