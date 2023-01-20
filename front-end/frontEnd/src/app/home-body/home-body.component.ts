8f9a55b6c3340cf91d774181983be7abe2524587
naolca <firaolibrahim28@gmail.com>
2023-01-21 00:09:46 +0300

    added some event bonding

--- front-end/frontEnd/src/app/home-body/home-body.component.ts
+++ front-end/frontEnd/src/app/home-body/home-body.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-body',
   templateUrl: './home-body.component.html',
   styleUrls: ['./home-body.component.css'],
 })
-export class HomeBodyComponent {
+export class HomeBodyComponent implements OnInit {
   options = ['ibuprofine', 'amoxaxiline', 'amoprazole'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  searchValue: string = '';
+
+  changeSearchValue(eventData: any) {
+    console.log(eventData);
+  }
 }
