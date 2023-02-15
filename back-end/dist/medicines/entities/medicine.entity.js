1ffa3f71b21dcddff6469771872b2757f69852ec
ffekirnew <ffekirnew0808@gmail.com>
2023-02-15 05:55:08 +0300

    renamed the folder to the standard "backend"

--- back-end/dist/medicines/entities/medicine.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Medicine = void 0;
-class Medicine {
-}
-exports.Medicine = Medicine;
-//# sourceMappingURL=medicine.entity.js.map
