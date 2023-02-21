3974c5899b683b34c08161c9812880dc5d6296b8
naolca <firaolibrahim28@gmail.com>
2023-02-21 04:42:53 +0300

    deleted the back end

--- backend/src/medicines/dto/get-medicines-filter.dto.ts
+++
@@ -1,3 +0,0 @@
-export class GetMedicinesFilterDto {
-    key: string;
-}
--- backend/src/privilages/dto/update-privilage.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePrivilageDto } from './create-privilage.dto';
-
-export class UpdatePrivilageDto extends PartialType(CreatePrivilageDto) {}
